e06705228e6117ec602086e27a278db48ef26d57
jelle van loock <r0484335@student.thomasmore.be>
2016-12-21 20:06:12 +0100

    4. POST Method klaar + validatie velden toegevoegd

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-
 // inladen van de dependencies
 var express = require('express'); // eenvoudige webserver in node js
 var parser = require('body-parser'); // extensie op express voor eenvoudig body uit te lezen
-var mongoose = require("mongoose"); // Aangeraden door Jonas Cristens (andere package dan MongoClient)
+var mongoose = require('mongoose'); // Aangeraden door Jonas Cristens (andere package dan MongoClient)
+
 mongoose.connect('mongodb://localhost:27017/MijnAPI');
 
 // onze lokale 'datastore'. deze variable bewaart onze state.
-var dalLocatie = require("./storagelocaties.js");
+var dalLocatie = require('./storagelocaties.js');
+var validationlocaties = require('./validatelocaties.js');
 
 // aanmaken van de webserver variabele
 var app = express();
@@ -21,12 +22,10 @@ app.get('/locaties', function (request, response) {
         }
         response.send(locatie);
     });
-
 });
 
 // opvangen van een GET op /locaties/:naam_drone
-app.get("/locaties/:id", function (request, response) {
-
+app.get('/locaties/:id', function (request, response) {
     dalLocatie.findLocaties(request.params.id, function (err, locatie) {
         if(err){
             throw err;
@@ -35,7 +34,38 @@ app.get("/locaties/:id", function (request, response) {
     });
 });
 
-
+// opvangen van een POST op /locaties.
+app.post("/locaties", function(request, response) {
+    // de data in de body wordt toegekend aan onze locatie variabele.
+    // deze is enkel opgevuld indien het JSON is.
+    var locatie = request.body;
+    // Valideren dat velden bestaan
+    var errors = validationlocaties.fieldsNotEmpty(locatie, "naam_drone", "mac_address_drone", "naam_locatie", "beschrijving");
+    if (errors) {
+        response.status(400).send({
+            msg: "Volgende velden zijn verplicht of fout: " + errors.concat()
+        });
+        return;
+    }
+    /*
+    // Valideren dat we niet dezelfde locatie 2x hebben
+    var existingLocatie = dal.findLocatie(locatie.naam_drone);
+    if (existingLocatie) {
+        response.status(409).send({
+            msg: "Naam_drone moet uniek zijn!",
+            link: "../locaties/" + existingLocatie.id
+        });
+        return;
+    }
+    Dit hoeft niet meer omdat we met moongoose zeggen dat ze uniek of niet uniek moeten zijn
+    */
+    dalLocatie.saveLocaties(locatie, function(err, locatie) {
+        if(err){
+            throw err
+        }
+        response.send(locatie);
+    });
+});
 
 // de server starten op poort 4567
 app.listen(4567);
