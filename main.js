1f307a11c4da7296bae0b4cba951d940a4d91d23
jelle van loock <r0484335@student.thomasmore.be>
2016-12-21 18:58:00 +0100

    2. HTTP Server: modules geïnstalleerd + storage file

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-console.log("Hello World!")
+
+// inladen van de dependencies
+var express = require('express'); // eenvoudige webserver in node js
+var parser = require('body-parser'); // extensie op express voor eenvoudig body uit te lezen
+
+// onze lokale 'datastore'. deze variable bewaart onze state.
+var dal = require("./storage.js");
+
+// aanmaken van de webserver variabele
+var app = express();
+// automatische json-body parsers van request MET media-type application/json gespecifieerd in de request.
+app.use(parser.json());
+
+// de server starten op poort 4567
+app.listen(4567);
+
+// Bevestiging
+console.log("Server started");
 
